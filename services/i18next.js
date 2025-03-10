ef238d2b2ece9e3440423f2626ac2944678fc058
xsamtech <124665689+xsamtech@users.noreply.github.com>
2025-03-10 18:21:43 +0100

    n

---
+++ services/i18next.js
@@ -0,0 +1,22 @@
+/**
+ * @author Xanders
+ * @see https://team.xsamtech.com/xanderssamoth
+ */
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import en from '../locales/en.json'
+import fr from '../locales/fr.json'
+
+export const languageResources = {
+    en: {translation: en},
+    fr: {translation: fr}
+}
+
+i18next.use(initReactI18next).init({
+    compatibilityJSON: 'v3',
+    lng: 'fr',
+    fallbackLng: 'en',
+    resources: languageResources
+});
+
+export default i18next;
---
+++ tools/constants.js
@@ -0,0 +1,58 @@
+/**
+ * @author Xanders
+ * @see https://team.xsamtech.com/xanderssamoth
+ */
+export const COLORS = {
+    danger: '#db3337',
+    light_danger: '#dbbbbf',
+    dark_danger: '#b91115',
+    primary: '#3a7ced',
+    light_primary: '#99aaff',
+    dark_primary: '#185acb',
+    secondary: '#d3d3d3',
+    dark_secondary: '#777',
+    info: '#3dbced',
+    success: '#2ba14d',
+    warning: '#ecad26',
+    light: '#f3f3f3',
+    dark_light: '#e3e3e3',
+    white: '#fff',
+    black: '#000',
+    iconColor: '#eaebef'
+};
+
+export const ICON_SIZE = {
+    s0_3: 45,
+    s0_4: 40,
+    s0_5: 35,
+    s1: 25,
+    s2: 23,
+    s3: 21,
+    s4: 19,
+    s5: 16,
+    s6: 14,
+};
+
+export const TEXT_SIZE = {
+    title: 22,
+    secondary: 14
+};
+
+export const PADDING = {
+    horizontal: 10,
+    vertical: 10
+};
+
+export const API = {
+    // url: 'http://192.168.43.78/boongo/public/api'
+    url: 'https://boongo7.com/api'
+};
+
+export const WEB = {
+    // url: 'http://192.168.43.78/boongo/public'
+    url: 'https://boongo7.com'
+};
+
+export const PHONE = {
+    admin: '+243815737600'
+};
